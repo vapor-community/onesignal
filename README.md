f53da93337fa477be783e7eba5373a731e1127ab
vkill <vkill.net@gmail.com>
2019-08-22 19:50:40 +0800

    Add tests for OneSignalNotification

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Using OneSignal on Vapor.
 For sending to an array of device tokens:
 
 ```swift
+let apiKey = "YourApiKey"
+let appId = "YourAppId"
+
 let deviceTokens = ["foo...", "bar..."]
 let message = OneSignalMessage("Hello Vapor!")
-let notif = OneSignalNotification(message: message, iosDeviceTokens: [deviceTokens])
-let app = OneSignalApp(apiKey: Environment.get("ONESIGNAL_API_KEY") ?? "", appId: Environment.get("ONESIGNAL_APP_ID") ?? "")
-let result = try OneSignal.makeService(for: request).send(notification: notif, toApp: app)
+let notif = OneSignalNotification(message: message, users: deviceTokens)
+let app = OneSignalApp(apiKey: apiKey, appId: appId)
+
+let resultFuture = try OneSignal().send(notification: notif, toApp: app)
 ```
--- Tests/OneSignalTests/OneSignalTests.swift
+++ Tests/OneSignalTests/OneSignalTests.swift
@@ -2,5 +2,22 @@
 import XCTest
 
 final class OneSignalTests: XCTestCase {
-    func testExample() {}
+    func testOneSignalNotificationGenerateRequest() throws {
+        let apiKey = "YourApiKey"
+        let appId = "YourAppId"
+
+        let deviceTokens = ["foo...", "bar..."]
+        let message = OneSignalMessage("Hello Vapor!")
+        let notif = OneSignalNotification(message: message, users: deviceTokens)
+        let app = OneSignalApp(apiKey: apiKey, appId: appId)
+
+        let request = try notif.generateRequest(for: app)
+
+        XCTAssertEqual(request.url.absoluteString, "https://onesignal.com/api/v1/notifications")
+
+        XCTAssertEqual(request.headers["Authorization"].first, "Basic YourApiKey")
+        XCTAssertEqual(request.headers["Content-Type"].first, "application/json")
+
+        XCTAssertEqual(request.body?.length, #"{"contents":{"en":"Hello Vapor!"},"app_id":"YourAppId","include_player_ids":["foo...","bar..."]}"# .count)
+    }
 }
