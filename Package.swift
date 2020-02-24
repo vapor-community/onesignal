597bc7e54bb02bcab8f8f56706275daad9372cd0
Anthony Castelli <hello@anthonyc.dev>
2020-02-24 11:34:24 -0800

    platforms: Remove the platform restriction

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "OneSignal",
-    platforms: [
-        .macOS(.v10_14)
-    ],
     products: [
         .library(name: "OneSignal", targets: ["OneSignal"]),
     ],
